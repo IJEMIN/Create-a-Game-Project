91b72de1ae2790b9a5d5ed25200a67d0a25d07a7
I_Jemin <i_jemin@hotmail.com>
2018-10-13 23:43:20 +0900

    Replace OnLevelWasLoaded to SceneManager.sceneLoaded event

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -3,39 +3,41 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour {
+    public AudioClip mainTheme;
+    public AudioClip menuTheme;
 
-	public AudioClip mainTheme;
-	public AudioClip menuTheme;
+    string sceneName;
 
-	string sceneName;
+    void Awake() {
+        SceneManager.sceneLoaded += OnNewSceneLoad;
+    }
 
-	void Start() {
-		OnLevelWasLoaded (0);
-	}
+    void OnNewSceneLoad(Scene scene, LoadSceneMode mode) {
+        string newSceneName = SceneManager.GetActiveScene().name;
 
+        if (newSceneName != sceneName)
+        {
+            sceneName = newSceneName;
+            Invoke("PlayMusic", .2f);
+        }
+    }
 
-	void OnLevelWasLoaded(int sceneIndex) {
-		string newSceneName = SceneManager.GetActiveScene ().name;
-		if (newSceneName != sceneName) {
-			sceneName = newSceneName;
-			Invoke ("PlayMusic", .2f);
-		}
-	}
+    void PlayMusic() {
+        AudioClip clipToPlay = null;
 
-	void PlayMusic() {
-		AudioClip clipToPlay = null;
+        if (sceneName == "Menu")
+        {
+            clipToPlay = menuTheme;
+        }
+        else if (sceneName == "Game")
+        {
+            clipToPlay = mainTheme;
+        }
 
-		if (sceneName == "Menu") {
-			clipToPlay = menuTheme;
-		} else if (sceneName == "Game") {
-			clipToPlay = mainTheme;
-		}
-
-		if (clipToPlay != null) {
-			AudioManager.instance.PlayMusic (clipToPlay, 2);
-			Invoke ("PlayMusic", clipToPlay.length);
-		}
-
-	}
-
+        if (clipToPlay != null)
+        {
+            AudioManager.instance.PlayMusic(clipToPlay, 2);
+            Invoke("PlayMusic", clipToPlay.length);
+        }
+    }
 }
